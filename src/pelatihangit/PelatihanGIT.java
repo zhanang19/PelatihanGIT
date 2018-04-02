9312f0116998b42e47f0c8773de74ce736a27ebe
Windows <Windows@DESKTOP-BBNIP3L>
2018-04-02 09:58:49 +0700

    Tugas Pak Robby Final

--- src/pelatihangit/PelatihanGIT.java
+++ src/pelatihangit/PelatihanGIT.java
@@ -7,43 +7,45 @@ import java.util.Scanner;
  * @author zhanang19
  */
 public class PelatihanGIT {
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Jumlah data : ");
-        int jumlahArray = scan.nextInt();
-        int [] dataArray = new int[jumlahArray];
+        int jumlahArray;
+        int tempData;
+        int[] dataArray;
         int totalData = 0;
-        boolean error = false;
+        boolean error;
         int dataMaximum = 0;
-        int dataMinimum = 2147483647;
+        int dataMinimum = 100;
+
+        System.out.print("Jumlah data : ");
+        jumlahArray = scan.nextInt();
+        dataArray = new int[jumlahArray];
         for (int i = 0; i < jumlahArray; i++) {
             do {
-                System.out.print("Masukkan data ke-" + (i+1) + " : ");
-                int tempData = scan.nextInt();
-                    if (tempData <= 100) {
-                        dataArray[i] = tempData;
-                        totalData += tempData;
-                        error = false;
-                    }else{
-                        System.out.println("Input salah!");
-                        error = true;
-                    }
+                System.out.print("Masukkan data ke-" + (i + 1) + " : ");
+                tempData = scan.nextInt();
+                if (tempData <= 100 && tempData > 0) {
+                    dataArray[i] = tempData;
+                    totalData += tempData;
+                    error = false;
+                } else {
+                    System.out.println("Input salah! Masukkan dalam range 1-100!");
+                    error = true;
+                }
             } while (error);
             if (dataArray[i] > dataMaximum) {
                 dataMaximum = dataArray[i];
-            }
-            if (dataArray[i] < dataMinimum) {
+            } else if (dataArray[i] < dataMinimum) {
                 dataMinimum = dataArray[i];
             }
         }
         for (int i = 0; i < jumlahArray; i++) {
-            System.out.println("Data ke " + (i+1) + " : " + dataArray[i]);
+            System.out.println("Data ke " + (i + 1) + " : " + dataArray[i]);
         }
         System.out.println("Data Maksimum : " + dataMaximum);
         System.out.println("Data Minimum : " + dataMinimum);
-        System.out.println("Rata-rata : " + (totalData/jumlahArray));
-        
-        
+        System.out.println("Rata-rata : " + (totalData / jumlahArray));
     }
-    
+
 }
